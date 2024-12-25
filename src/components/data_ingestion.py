5705f603902971f6074f998910b15edcec8ff2c4
sivabalan2311 <sivabalan2311@gmail.com>
2024-12-25 13:28:37 +0530

    Data transform and trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,12 +7,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str=os.path.join('artifact','train.csv')
-    test_data_path:str=os.path.join('artifact','test.csv')
-    raw_data_path:str=os.path.join('artifact','raw.csv')
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','raw.csv')
 
 class DataIngestion:
     def __init__(self):
@@ -53,4 +59,10 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initate_data_ingestion()
+    train_data, test_data,_= obj.initate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
